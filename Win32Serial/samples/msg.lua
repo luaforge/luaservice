87a0a6e7bef80740a090c433adaa02015b487291
Ross Berteig <rberteig@luaforge.net>
2008-06-25 02:39:11 +0000

    Initial revision.

---
+++ Win32Serial/samples/msg.lua
@@ -0,0 +1,16 @@
+require "win32ser"
+hw = win32ser.GetActiveWindow()
+yesno = win32ser.MessageBoxA(hw,
+	"Happy Now?",
+	"Hello There Active",
+	win32ser.MB_YESNO+win32ser.MB_ICONSTOP)
+hw = win32ser.GetForegroundWindow()
+yesno = win32ser.MessageBoxA(hw,
+	"Happy Now?",
+	"Hello There Foreground",
+	win32ser.MB_YESNO+win32ser.MB_ICONINFORMATION)
+print("yesno", yesno)
+
+if not win32ser.MessageBeep(win32ser.MB_ICONASTERISK) then
+    print ("Error", win32ser.GetLastError())
+end
